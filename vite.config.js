754e0d02982b99f8e97114704f38ce9e07136d70
Jonatandb <jonatandb@gmail.com>
2024-08-31 23:39:06 -0300

    Simplified vite config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: "/my-practices-tailwindcss-2024-08/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: './main.js',
-        index: './index.html',
-      },
-    },
-  },
 });
